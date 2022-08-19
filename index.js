e63cef52047980c0156c2d7ec03774c62ab8b1cd
Sean Gillick <seanpgillick@gmail.com>
2022-08-19 15:58:54 -0400

    Basic header added

--- index.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {    // this will work when your window scrolled.
-    var height = $(window).scrollTop();  //getting the scrolling height of window
-    console.log("Scroll");
-    if(height  > 100) {
-        $(".header").css({"position": "fixed"});
-    } else{
-        $(".header").css({"position": "relative"});
-    }
-});
