e29d45ce1462aab1de2df10d827188c5b4144e96
Sean Gillick <seanpgillick@gmail.com>
2022-08-20 12:53:58 -0400

    Migrated everything into public_html folder

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+app.use(express.static("public_html"));
+
+
 app.get("/", function (req, res){
     res.send("It worked!");
 })
