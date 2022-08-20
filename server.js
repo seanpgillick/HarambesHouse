a440a7649b39ff2278e17b1f0647e0fc0e47e906
seanpgillick <75447516+seanpgillick@users.noreply.github.com>
2022-08-20 16:08:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.get("/new", function (req, res){
     res.send("New also worked!");
 })
 
-app.listen(port, () => {
-    console.log(`Listening at port: ${port}!!! :)`);
-}); 
-///////////////////////////
-app.get('/', (req, res) => {
-    res.send("Welcome to the home page")
-});
+// app.listen(port, () => {
+//     console.log(`Listening at port: ${port}!!! :)`);
+// }); 
+// ///////////////////////////
+// app.get('/', (req, res) => {
+//     res.send("Welcome to the home page")
+// });
 
 app.listen(process.env.PORT || 5000);
 module .export = app; 
