fc98baad638ff64aeac234f1977e653f787ca44c
Sean Gillick <seanpgillick@gmail.com>
2022-08-20 12:49:13 -0400

    Testing new page

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ app.get("/", function (req, res){
     res.send("It worked!");
 })
 
+app.get("/new", function (req, res){
+    res.send("New also worked!");
+})
+
 app.listen(process.env.PORT || 5000);
 module .export = app;
